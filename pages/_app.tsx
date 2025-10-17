7b8a3c994436ce169e1d92124cf1689c50fb23f9
unknown <bryanthuamcorporativo@gmail.com>
2025-10-17 15:17:42 -0500

    fix: correct CSS import path in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app';
-import '../globals.css'; // tenemos globals.css en la raíz (lo trajo create-next-app)
+import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
