1d8afd9d8d80132243e1054f6bfd2856b4ad2ef9
unknown <bryanthuamcorporativo@gmail.com>
2025-10-27 00:55:21 -0500

    chore: remove old auth-helpers and use plain _app with @supabase/ssr setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,7 @@
 // /pages/_app.tsx
-import { useState } from 'react';
 import type { AppProps } from 'next/app';
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
-import { SessionContextProvider } from '@supabase/auth-helpers-react';
 import '../styles/globals.css';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
-  return (
-    <SessionContextProvider supabaseClient={supabaseClient} initialSession={pageProps.initialSession}>
-      <Component {...pageProps} />
-    </SessionContextProvider>
-  );
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
